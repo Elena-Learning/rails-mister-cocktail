8cc1b0a225c04d475b47c4184e2839f1bd5ab980
Elena-Learning <elena.urban.professionell@gmail.com>
2019-11-14 14:58:47 +0100

    commit step in between, controllers created

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,15 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
